b26ac1ecec21a88019044fbcf53a507dc47f7e0f
Nataliya Borisova <natalyaka25@yandex.ru>
2026-05-06 21:42:05 +0500

    Исправлено подключение к MongoDB для Docker/CI

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,2 +1,2 @@
-export const PORT = 3000;
-export const DB_ADDRESS = 'mongodb://127.0.0.1:27017/weblarek';
+export const PORT = Number(process.env.PORT) || 3000;
+export const DB_ADDRESS = process.env.DB_ADDRESS || 'mongodb://127.0.0.1:27017/weblarek';
